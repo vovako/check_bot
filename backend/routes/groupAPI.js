b63325f3b2f77056f581178d5a2f4d688da3924a
izumra <zagumennikovmark@gmail.com>
2023-09-11 09:57:04 +0500

    asda

--- backend/routes/groupAPI.js
+++ backend/routes/groupAPI.js
@@ -1,13 +1,19 @@
 import express from 'express'
 import { query } from '../db.js'
+import { randomUUID } from 'crypto'
 
 const api=express.Router()
 
-api.get('/',async(req,res)=>{
+api.post('/',express.json(),async(req,res)=>{
     let groups=await query("select * from Class")
     for(let i=0;i<groups?.length;i++){
         groups[i].participants=(await query("select count(*) as participants from Participant where id_group=?",[groups[i].ID_group]))[0].participants
-        groups[i].omissions=(await query("select count(distinct Omission.id_participant) as omissions from Omission left join Participant on Omission.id_participant=Participant.id_participant where Participant.id_group=?",[groups[i].ID_group]))[0].omissions
+        groups[i].omissions=(await query("select count(distinct Omission.id_participant) as omissions from Omission left join Participant on Omission.id_participant=Participant.id_participant where Participant.id_group=? and day(date_omission)=day(now())",[groups[i].ID_group]))[0].omissions
+        if(req?.body?.date){
+            let date=new Date(req?.body?.date)
+            console.log(date)
+            if(date)groups[i].ismarked=(await query("select exists(select id_group from Record where day(date_rec)=day(?) and id_group=?) as marked",[req?.body?.date,groups[i].id_group]))[0].marked
+        }
     }
 
     if(groups?.length)res.status(200).json({code:200,message:"Информация о группах получена",data:groups})
@@ -16,11 +22,10 @@ api.get('/',async(req,res)=>{
 
 api.get('/:id',async(req,res)=>{
     let group=await query("select * from Class where id_group=?",[req.params.id])
-
     if(group?.length){
-        group[0].participants=await query("select * from Participant where id_group=?",[group[0].ID_group])
-        group[0].omissions=await query("select distinct Omission.id_participant,Participant.* from Omission left join Participant on Omission.id_participant=Participant.id_participant where Participant.id_group=?",[group[0].ID_group])
-        res.status(200).json({code:200,message:"Информация о группах получена",data:group})
+        group[0].participants=await query("select Participant.id_participant,Participant.fullname from Participant where id_group=?",[group[0].ID_group])
+        group[0].omissions=await query("select distinct Omission.id_participant,Participant.id_participant,Participant.fullname from Omission left join Participant on Omission.id_participant=Participant.id_participant where Participant.id_group=? and day(date_omission)=day(now())",[group[0].ID_group])
+        res.status(200).json({code:200,message:"Информация о группе получена",data:group})
     }
     else res.status(404).json({code:404,message:"Групп не зарегестрировано",data:[]})
 })
--- backend/routes/recordAPI.js
+++ backend/routes/recordAPI.js
@@ -5,15 +5,30 @@ import { randomUUID } from 'crypto'
 
 const api=express.Router()
 
-api.post('/',async(req,res)=>{
-    if(req.body?.participants?.length){
-        let key=randomUUID()
-        await query("insert into Record(id_record,id_duty,id_group)values(?,?)",[key,req.body?.id_duty,req.body.id_group])
-        for(let i=0;i<req.body?.participants?.length;i++){
-            await query('insert into Omission(id_participant,date_omission)')
+
+api.post('/getstats',async(req,res)=>{
+    if(req?.body?.date){
+        let date=new Date(req?.body?.date)
+        if(date){
+            let groups=await query("select * from Omission")
+            for(let i=0;i<groups?.length;i++){
+                groups[i].participants=(await query("select count(*) as participants from Participant where id_group=?",[groups[i].ID_group]))[0].participants
+                groups[i].omissions=(await query("select count(distinct Omission.id_participant) as omissions from Omission left join Participant on Omission.id_participant=Participant.id_participant where Participant.id_group=? and day(date_omission)=day(now())",[groups[i].ID_group]))[0].omissions
+            }
         }
+        res.status(500).json({code:500,message:"Был передан неверный формат даты",data:[]})
     }
     else res.status(400).json({code:400,message:"Не был передан список отсутствующих",data:[]})
+})
+
+api.post('/addrecord',express.json(),async(req,res)=>{
+    if(req.body?.participants?.length&&req.body?.id_group){
+        let key=randomUUID()
+        await query("insert into Record(id_record,id_duty,id_group)values(?,?)",[key,req.body?.id_duty,req.body?.id_group])
+        for(let i=0;i<req.body?.participants?.length;i++)await query('insert into Omission(id_participant,id_record)',[req.body?.participants[i].id_participant,key])
+        res.status(200).json({code:200,message:"Запись отсутствующих в группе осуществлена",data:{id_record:key}})
+    }
+    else res.status(400).json({code:400,message:"Не был передан список отсутствующих или идентификатор группы",data:[]})
 })
 
 export default api
