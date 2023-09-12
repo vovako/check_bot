340e8fe6d72ab5e367171cb62d7316303817b99e
sisya228 <huitebevrylo@mail.ru>
2023-09-12 11:46:44 +0300

    fix

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,64 @@
-// import WebApp from '@grammyjs/web-app';
-// WebApp.ready();
+const ORIGIN = 'http://192.168.100.71:3000'
+let GROUPS_DATA = {}
+
+const curDate = new Date().toISOString().split('T')[0]
+
+const getGroupsRaw = {
+	"date": curDate
+}
+fetch(`${ORIGIN}/api/groups/getgroups`, {
+	method: 'post',	
+	body: JSON.stringify(getGroupsRaw),
+	headers: {
+		'Content-Type': 'application/json'
+	}
+}).then(res => res.json())
+	.then(json => {
+		console.log(json)
+
+		if (json.code === 200) {
+			GROUPS_DATA = json.data
+			const writePage = document.querySelector('.write')
+			const historyPage = document.querySelector('.history')
+
+			GROUPS_DATA.forEach(group => {
+				const omissions = group.records.length ? group.records.pop().absent : 0
+				const cameCount = group.participants - +omissions
+
+				const course = group.id_group[0]
+				const writeBox = writePage.querySelector(`.groups__course[data-course="${course}"]`)
+				writeBox.insertAdjacentHTML('beforeend', `
+				<div class="groups-item">
+					<div class="groups-item__title">${group.id_group}</div>
+					<div class="groups-item__count">
+						<span class="cur">${cameCount}</span> /
+						<span class="all">${group.participants}</span>
+					</div>
+				</div>
+				`)
+
+				const historyBox = historyPage.querySelector(`.groups__course[data-course="${course}"]`)
+				const curatorParts = group.teacher.split(' ')
+				const curatorName = `${curatorParts[0]} ${curatorParts[1][0]} ${curatorParts[2][0]}`;
+				
+				historyBox.insertAdjacentHTML('beforeend', `
+				<tr>
+					<td rowspan="2">
+						<span class="group">${group.id_group}</span> <br>
+						<span class="curator">${curatorName}</span>
+					</td>
+					<td rowspan="2">${group.participants}</td>
+					<td>${group.records[0]?.time.slice(0, -3) ?? '-'}</td>
+					<td>${group.records[0]?.absent ?? '-'}</td>
+				</tr>
+				<tr>
+					<td>${group.records[1]?.time.slice(0, -3) ?? '-'}</td>
+					<td>${group.records[1]?.absent ?? '-'}</td>
+				</tr>
+				`)
+			});
+		}
+	});
 
 document.addEventListener('click', function (evt) {
 	const target = evt.target
@@ -8,12 +67,45 @@ document.addEventListener('click', function (evt) {
 		const targetPage = target.getAttribute('data-page-target')
 		toPage(targetPage)
 	} else if (target.classList.contains('groups-item')) {
+
 		const section = target.closest('section')
+
 		if (section.classList.contains('write')) {
+			const group = target.querySelector('.groups-item__title').textContent
+			const groupCrumb = [...document.querySelector('.write-group .breadcrumbs').children].pop()
+			groupCrumb.textContent = group
+
+			const writeGroupConut = document.querySelector('.write-group__count')
+			const groupData = GROUPS_DATA.filter(g => g.id_group === group)[0]
+			const cameCount = target.querySelector('.groups-item__count .cur').textContent
+
+			writeGroupConut.outerHTML = `
+				<div class="write-group__count"><span class="cur"><input max="${groupData.participants}" min="0" type="number" value="${cameCount}"
+					inputmode="numeric" oninput="{
+						let value = this.value
+						while (Number(value) > Number(this.max)) {
+							value = value.slice(0, -1)
+						}
+						this.value = value
+						return false;
+						}"
+				onfocus="this.select()"
+				onfocusout="{
+					if (this.value === '') {
+						this.value = 0
+					}
+				}"></span>
+				/ <span class="all">${groupData.participants}</span></div>
+			`;
+
 			toPage('write-group')
+
 		} else if (section.classList.contains('history')) {
+
 			toPage('history-group')
+
 		}
+
 	} else if (!target.closest('.select-with-image') && document.querySelector('.select-with-image__list.active')) {
 		const allSelect = document.querySelectorAll('.select-with-image__list')
 		Array.from(allSelect).map(e => e.classList.remove('active'))
